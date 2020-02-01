00c5905e64ba413114bb834fc83665deb67e9238
kkmitra <kaushik.mitra@innoraft.com>
2020-02-01 12:39:21 +0530

    Solved Problem 6

--- custom.js
+++ custom.js
@@ -14,4 +14,16 @@ window.addEventListener("load", function() {
     })
 
 
+    // Problem 6
+    document.querySelector("#problem_6_btn").addEventListener("click", function() {
+        document.querySelectorAll(".section6 > p")
+            .forEach(function(e) {
+                if(e.classList.value === "") {
+                    e.style.backgroundColor = "#fff";
+                }
+            })
+    })
+
+
+
 })
