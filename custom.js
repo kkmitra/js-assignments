c892ff455f99ff1f5268a71a80e576ea088e4024
kkmitra <kaushik.mitra@innoraft.com>
2020-02-01 12:13:24 +0530

    Solved Problem 1

---
+++ custom.js
@@ -0,0 +1,12 @@
+window.addEventListener("load", function() {
+    
+
+    // Problem 1
+    document.querySelector("#problem_1_btn").addEventListener("click", function() {
+        document.querySelector(".section1 .height")
+            .style.height = '300px'
+    })
+
+
+
+})
