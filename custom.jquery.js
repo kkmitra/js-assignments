52f06a492db071990dd854f735c869faf437e8c4
kkmitra <kaushik.mitra@innoraft.com>
2020-02-03 17:47:46 +0530

    Solved Problem 2

--- custom.jquery.js
+++ custom.jquery.js
@@ -1,15 +1,15 @@
 $(document).ready(function() {
   // Problem 1
   $("#problem_1_btn").click(function() {
-      $(".section1 .height").animate({
-        height: "300px"
-      })
-  })
+    $(".section1 .height").animate({
+      height: "300px"
+    });
+  });
 
   // Problem 2
   $("#problem_2_btn").click(function() {
     $(".section2 .height").animate({
       marginLeft: "300px"
-    })
-})
+    });
+  });
 });
