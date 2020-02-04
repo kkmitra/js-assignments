be8d532164d61e20fe4ed9b2a725999f6006b977
kkmitra <kaushik.mitra@innoraft.com>
2020-02-04 10:56:57 +0530

    Solved Problem 6

--- custom.jquery.js
+++ custom.jquery.js
@@ -35,4 +35,15 @@ $(document).ready(function() {
       .nextUntil("li:eq(6)")
       .css("background-color", "#ff0000");
   });
+
+  // Problem 6
+  $("#problem_6_btn").click(function() {
+    $(".section6")
+      .find("p")
+      .each(function() {
+        if(!$(this).attr('class')) {
+          $(this).css("background-color", "#fff");
+        }
+      })
+  });
 });
